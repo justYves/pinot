6d983c748f0ea9068eee2d8c2a0af761bd803a6d
Yves Yuen <yyuen@linkedin.com>
2017-03-02 00:40:12 -0800

    working local script

--- thirdeye/thirdeye-pinot/src/main/resources/scripts/getGraphPnj.js
+++ thirdeye/thirdeye-pinot/src/main/resources/scripts/getGraphPnj.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var page = require('webpage').create();
 var system = require('system');
 var args = require('system').args;
@@ -6,9 +8,70 @@ page.viewportSize = {
   width: 480,
   height: 800
 };
+page.settings.resourceTimeout = 1000000;
 
+var requestsArray = [];
 
-page.open('http://lva1-app0583.corp.linkedin.com:1426/thirdeye#anomalies?anomaliesSearchMode=id&pageNumber=1&anomalyIds=2828648', function(status) {
+// recording navigation request
+page.onNavigationRequested = function(url, type, willNavigate, main) {
+  system.stderr.writeLine('= onNavigationRequested()');
+  system.stderr.writeLine(' Trying to navigate to: ' + url);
+  system.stderr.writeLine(' Caused by: ' + type);
+  system.stderr.writeLine(' Will actually navigate: ' + willNavigate);
+  system.stderr.writeLine(' Sent from the page\'s main frame: ' + main);
+};
+
+// recording resource loading errors; exit when operations are cancelled
+page.onResourceError = function(resourceError) {
+  system.stderr.writeLine('= onResourceError()');
+  system.stderr.writeLine(' Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+  system.stderr.writeLine(' Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+};
+
+// on resource requested
+page.onResourceRequested = function(requestData, networkRequest) {
+  system.stderr.writeLine('= onResourceRequested()');
+  requestsArray.push(requestData.id);
+  system.stderr.writeLine(requestsArray.length + ' pending requests');
+  system.stderr.writeLine(' Request (#' + requestData.id + '): ' + 'url ' +requestData.url);
+};
+
+page.onResourceReceived = function (response) {
+  var index = requestsArray.indexOf(response.id);
+  if (index !== -1) {
+    requestsArray.splice(index, 1);
+  }
+  system.stderr.writeLine('= onResourceReceived()');
+  system.stderr.writeLine(' Request (#' + response.id + '): ');
+  system.stderr.writeLine(requestsArray.length + ' pending requests');
+};
+
+// function onPageReady() {
+//     var htmlContent = page.evaluate(function () {
+//         return document.documentElement.outerHTML;
+//     });
+
+//     console.log(htmlContent);
+
+//     phantom.exit();
+// }
+
+page.open('http://localhost:1426/thirdeye#anomalies?anomaliesSearchMode=id&pageNumber=1&anomalyIds=2828648', function(status) {
+  // function checkReadyState() {
+  //       setTimeout(function () {
+  //           var readyState = page.evaluate(function () {
+  //               return document.readyState;
+  //           });
+
+  //           if ("complete" === readyState) {
+  //               onPageReady();
+  //           } else {
+  //               checkReadyState();
+  //           }
+  //       });
+  //   }
+
+    // checkReadyState();
   system.stderr.writeLine('= onOpen()');
   system.stderr.writeLine(' PhantomJS version: ' + JSON.stringify(phantom.version));
   system.stderr.writeLine(' PhantomJS page settings: ' + JSON.stringify(page.settings));
@@ -18,11 +81,28 @@ page.open('http://lva1-app0583.corp.linkedin.com:1426/thirdeye#anomalies?anomali
   }
 });
 
-// recording navigation request
-page.onNavigationRequested = function(url, type, willNavigate, main) {
-  system.stderr.writeLine('= onNavigationRequested()');
-  system.stderr.writeLine(' Trying to navigate to: ' + url);
-  system.stderr.writeLine(' Caused by: ' + type);
-  system.stderr.writeLine(' Will actually navigate: ' + willNavigate);
-  system.stderr.writeLine(' Sent from the page\'s main frame: ' + main);
+page.onLoadFinished = function(status) {
+  system.stderr.writeLine('= onLoadFinished()');
+  console.log('requestedArray');
+  console.log(requestsArray.length);
+
+  var interval = setInterval(function () {
+
+    if (requestsArray.length === 0) {
+      clearInterval(interval);
+      var clipRect = page.evaluate(function () { return document.querySelector('#anomaly-chart-0').getBoundingClientRect(); });
+      // var clipRect = document.querySelector('#anomaly-chart-0').getBoundingClientRect();
+      page.clipRect = {
+        top:    clipRect.top,
+        left:   clipRect.left,
+        width:  clipRect.width,
+        height: clipRect.height
+      };
+      page.render('capture.png');
+      phantom.exit();
+    }
+  }, 500);
+
+  interval();
+
 };
