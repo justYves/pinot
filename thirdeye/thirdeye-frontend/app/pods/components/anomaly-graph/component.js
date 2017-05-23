354d9f09eebe0522cd77e949a00ea9c507a4cc71
Yves Yuen <yyuen@linkedin.com>
2017-05-23 16:32:28 -0700

    added ember-c3 and componentized graph

--- thirdeye/thirdeye-frontend/app/pods/components/anomaly-graph/component.js
+++ thirdeye/thirdeye-frontend/app/pods/components/anomaly-graph/component.js
@@ -2,5 +2,42 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['anomaly-graph']
+  classNames: ['anomaly-graph'],
+  data: Ember.computed(
+    'anomaly',
+    function() {
+      return {
+        columns: [
+          ['date', ...this.get('anomaly.dates')],
+          ['current', ...this.get('anomaly.currentValues')],
+          ['baseline', ...this.get('anomaly.baselineValues')]
+        ],
+        type: 'line',
+        x: 'date',
+        xFormat: '%Y-%m-%d %H:%M'
+      }
+    }
+  ),
+  axis: {
+    y: {
+      show: true
+    }, 
+    x: {
+      type: 'timeseries',
+      show: true,
+      tick: {
+        fit: false
+      }
+    }
+  },
+  regions: Ember.computed('anomaly', function() {
+    return [{
+      axis: 'x',
+      start: this.get('anomaly.anomalyRegionStart'),
+      end: this.get('anomaly.anomalyRegionEnd'),
+      tick : {
+        format : '%m %d %Y'
+      }
+    }]
+  })
 });
