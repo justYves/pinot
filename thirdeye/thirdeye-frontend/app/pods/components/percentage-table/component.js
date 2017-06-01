0d23a27c5fe2991ea69b9f7c6da2271732caae18
Yves Yuen <yyuen@linkedin.com>
2017-05-31 22:33:12 -0700

    added percentage-table component

---
+++ thirdeye/thirdeye-frontend/app/pods/components/percentage-table/component.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+    dates: Ember.computed(
+        'metrics', 
+        function() {
+           return this.get('metrics.firstObject') && this.get('metrics.firstObject').timeBucketsCurrent;
+    }),
+    didReceiveAttrs() {
+        debugger;
+    }
+});
