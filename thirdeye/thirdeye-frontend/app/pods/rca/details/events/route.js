9fd604b24c8bd4672ccc5b262b598f70639d60ea
Yves Yuen <yyuen@linkedin.com>
2017-06-29 11:18:44 -0700

    hook up events

---
+++ thirdeye/thirdeye-frontend/app/pods/rca/details/events/route.js
@@ -0,0 +1,53 @@
+import Ember from 'ember';
+import moment from 'moment';
+import { Actions as MetricsActions } from 'thirdeye-frontend/actions/metrics';
+
+
+export default Ember.Route.extend({
+  // queryParams: {
+  //   startDate: {
+  //     refreshModel: true
+  //   },
+  //   endDate: {
+  //     refreshModel: true
+  //   },
+  //   granularity: {
+  //     refreshModel: true
+  //   },
+  //   filters: {
+  //     refreshModel: true
+  //   },
+  //   compareMode: {
+  //     refreshModel: true
+  //   }
+  // },
+
+  redux: Ember.inject.service(),
+
+  /**
+   * Massages Query Params from URL and dispatch redux actions
+   */
+  model(params, transition) {
+    // if (!params.id) { return; }
+    const { id: metricId } = params;
+
+    // // const redux = this.get('redux');
+    // const defaultQueryParams = {
+    //   startDate: moment().subtract(1, 'day').endOf('day'),
+    //   endDate: moment().subtract(1, 'week').endOf('day'),
+    //   granularity: 'DAYS',
+    //   filters: JSON.stringify({}),
+    // }
+    // const queryParams  = Object.assign(defaultQueryParams, transition.queryParams);
+    // const metricParams = Object.assign({}, params, queryParams)
+
+    return Ember.$.get(`/rootcause/query?framework=relatedEvents&current=1492564800000&baseline=1491960000000&windowSize=28200000&metricUrn=thirdeye:metric:${metricId}`)
+    // redux.dispatch(MetricsActions.setPrimaryMetric(metricParams))
+    //   .then((res) => redux.dispatch(MetricsActions.fetchRelatedMetricIds(res)))
+    //   .then((res) => redux.dispatch(MetricsActions.fetchRegions(res)))
+    //   .then((res) => redux.dispatch(MetricsActions.fetchRelatedMetricData(res)))
+    //   .catch((error) => redux.dispatch(MetricsActions.requestFail(error)));
+   
+    // return {};
+  }
+});
