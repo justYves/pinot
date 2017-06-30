0e435079f421c4d1bd54f4b26b82534f280263d3
Yves Yuen <yyuen@linkedin.com>
2017-06-30 10:03:07 -0700

    add wow to summary

--- thirdeye/thirdeye-frontend/app/pods/rca/details/controller.js
+++ thirdeye/thirdeye-frontend/app/pods/rca/details/controller.js
@@ -1,16 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['granularity', 'filter'],
+  queryParams: ['granularity', 'filter', 'compareMode'],
   granularities: Ember.computed.reads('model.granularities'),
   // granularity: Ember.computed.reads('granularities.firstObject'),
   noMatchesMessage: '',
   filters: JSON.stringify({}),
+
+  compareMode: 'WoW',
+  compareModeOptions: ['WoW', 'Wo2W', 'Wo3W', 'Wo4W'],
+
   metricFilters: Ember.computed.reads('model.metricFilters'),
 
   actions: {
     onGranularityChange(granularity) {
       this.set('granularity', granularity);
-    }
+    },
+    /**
+     * Changes the compare mode
+     * @param {String} compareMode baseline compare mode
+     */
+    onModeChange(compareMode){
+      this.set('compareMode', compareMode);
+    },
   }
 });
